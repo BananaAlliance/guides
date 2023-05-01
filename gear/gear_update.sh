97741f811a0ec9b22232f39fd38c22a08dfe39e9
nodespro <wladie.miller.dodo@gmail.com>
2023-05-01 21:55:01 +0300

    upd

--- gear/gear_update.sh
+++ gear/gear_update.sh
@@ -15,16 +15,38 @@ rm gear-nightly-linux-x86_64.tar.xz
 
 sudo systemctl start gear
 
-sleep 15
+sleep 1
 
-sed -i "s/gear-node/gear/" "/etc/systemd/system/gear.service"
+NODENAME_GEAR=$(grep -Po '(?<=--name\s)\S+(?=\s*--execution)' /etc/systemd/system/gear.service)
 
-sudo systemctl daemon-reload
-sudo systemctl stop gear
-cd /root/.local/share/gear/chains
-mkdir -p gear_staging_testnet_v6/network/
+sudo tee <<EOF >/dev/null /etc/systemd/system/gear.service
+[Unit]
+Description=Gear Node
+After=network.target
 
-sudo cp gear_staging_testnet_v6/network/secret_ed25519 gear_staging_testnet_v7/network/secret_ed25519  &>/dev/null
+[Service]
+Type=simple
+User=$USER
+WorkingDirectory=$HOME
+ExecStart=$HOME/gear \
+        --name $NODENAME_GEAR \
+        --execution wasm \
+        --port 31333 \
+        --telemetry-url 'ws://telemetry-backend-shard.gear-tech.io:32001/submit 0'
+
+
+Restart=always
+RestartSec=10
+LimitNOFILE=10000
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# cd /root/.local/share/gear/chains
+# mkdir -p gear_staging_testnet_v7/network/
+# sudo cp gear_staging_testnet_v6/network/secret_ed25519 gear_staging_testnet_v7/network/secret_ed25519  &>/dev/null
+
 
 sudo systemctl daemon-reload
 sudo systemctl restart gear
