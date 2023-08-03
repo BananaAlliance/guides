442e8f0fc717aef7e42684d579356cbcafba01d9
nodespro <wladie.miller.dodo@gmail.com>
2023-08-03 14:35:29 +0300

    upd

--- massa/massa_update.sh
+++ massa/massa_update.sh
@@ -29,8 +29,8 @@ function delete {
 }
 
 function install {
-    wget https://github.com/massalabs/massa/releases/download/TEST.24.1/massa_TEST.24.1_release_linux.tar.gz
-    tar zxvf massa_TEST.24.1_release_linux.tar.gz -C $HOME/
+    wget https://github.com/massalabs/massa/releases/download/TEST.25.1/massa_TEST.25.1_release_linux.tar.gz
+    tar zxvf massa_TEST.25.1_release_linux.tar.gz -C $HOME/
 }
 
 function routable_ip {
@@ -38,34 +38,10 @@ function routable_ip {
   sed -i "/\[network\]/a routable_ip=\"$(curl -s ifconfig.me)\"" "$HOME/massa/massa-node/base_config/config.toml"
 }
 
-function replace_bootstraps {
-  	config_path="$HOME/massa/massa-node/base_config/config.toml"
-  	bootstrap_list=`wget -qO- https://raw.githubusercontent.com/SecorD0/Massa/main/bootstrap_list.txt | shuf -n50 | awk '{ print "        "$0"," }'`
-  	len=`wc -l < "$config_path"`
-  	start=`grep -n bootstrap_list "$config_path" | cut -d: -f1`
-  	end=`grep -n "\[optionnal\] port on which to listen" "$config_path" | cut -d: -f1`
-  	end=$((end-1))
-  	first_part=`sed "${start},${len}d" "$config_path"`
-  	second_part="
-      bootstrap_list = [
-  ${bootstrap_list}
-      ]
-  "
-  	third_part=`sed "1,${end}d" "$config_path"`
-  	echo "${first_part}${second_part}${third_part}" > "$config_path"
-  	sed -i -e "s%retry_delay *=.*%retry_delay = 10000%; " "$config_path"
-}
-
 function keys_from_backup {
 	cp $HOME/massa_backup/wallet.dat $HOME/massa/massa-client/wallet.dat
 	cp $HOME/massa_backup/node_privkey.key $HOME/massa/massa-node/config/node_privkey.key
 }
-
-function alias {
-  echo "alias client='cd $HOME/massa/massa-client/ && $HOME/massa/massa-client/massa-client --pwd $massa_pass && cd'" >> ~/.profile
-  echo "alias clientw='cd $HOME/massa/massa-client/ && $HOME/massa/massa-client/massa-client --pwd $massa_pass && cd'" >> ~/.profile
-}
-
 
 get_env
 massa_backup
@@ -73,9 +49,6 @@ delete
 
 install
 routable_ip
-#replace_bootstraps
-# alias
-
 #keys_from_backup
 
 sudo systemctl start massa
