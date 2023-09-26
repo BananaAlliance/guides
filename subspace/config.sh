411e3dbde3c2d85f49ecd384902840f46eacc21d
sicmundu <grodstrike@gmail.com>
2023-09-26 21:43:06 +0400

    Update config.sh

--- subspace/config.sh
+++ subspace/config.sh
@@ -1,2 +1,2 @@
-PULSAR_URL="https://github.com/subspace/pulsar/releases/download/v0.6.9-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.9-alpha"
-CURRENT_VERSION="0.6.9"
+PULSAR_URL="https://github.com/subspace/pulsar/releases/download/v0.6.10-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.10-alpha"
+CURRENT_VERSION="0.6.10"
