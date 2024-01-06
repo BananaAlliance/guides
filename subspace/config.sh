855453a9ab6f5bd1a1a5097186d4e3eca4893209
sicmundu <grodstrike@gmail.com>
2024-01-06 15:44:44 +0400

    Update config.sh

--- subspace/config.sh
+++ subspace/config.sh
@@ -1,4 +1,4 @@
 PULSAR_URL="https://github.com/subspace/pulsar/releases/download/v0.6.14-alpha/pulsar-ubuntu-x86_64-skylake-v0.7.2-alpha"
 CURRENT_VERSION="0.6.14"
-SUBSPACE_NODE="https://github.com/subspace/subspace/releases/download/gemini-3g-2024-jan-03/subspace-node-ubuntu-x86_64-skylake-gemini-3g-2023-dec-01"
-SUBSPACE_FARMER="https://github.com/subspace/subspace/releases/download/gemini-3g-2024-jan-03/subspace-farmer-ubuntu-x86_64-skylake-gemini-3g-2023-dec-01"
+SUBSPACE_NODE="https://github.com/subspace/subspace/releases/download/gemini-3g-2024-jan-03/subspace-node-ubuntu-x86_64-skylake-gemini-3g-2024-jan-03"
+SUBSPACE_FARMER="https://github.com/subspace/subspace/releases/download/gemini-3g-2024-jan-03/subspace-farmer-ubuntu-x86_64-skylake-gemini-3g-2024-jan-03"
