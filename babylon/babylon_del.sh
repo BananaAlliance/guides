2db9da6f22b799cd75c255d3f175a4d8b6d6eb0f
sicmundu <grodstrike@gmail.com>
2024-02-25 10:42:14 +0400

    Update babylon_del.sh

--- babylon/babylon_del.sh
+++ babylon/babylon_del.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Найти и убить процессы, связанные с 'babylon' и 'babylond'
 pgrep -f 'babylon' | xargs -r kill -9
 pgrep -f 'babylond' | xargs -r kill -9
 
-# Найти и удалить файлы и папки, содержащие 'babylon' и 'babylond' в их названиях
-# ВНИМАНИЕ: Данная команда может быть опасной, так как она удалит все найденные файлы без подтверждения.
-# Рекомендуется сначала выполнить команду без 'rm -rf' для проверки, какие файлы будут удалены.
+
 find / -type f -name '*babylon*' -exec rm -rf {} +
 find / -type d -name '*babylon*' -exec rm -rf {} +
 find / -type f -name '*babylond*' -exec rm -rf {} +
