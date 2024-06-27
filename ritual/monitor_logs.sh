ebacae41170b42d7395ee2f6451ced2cb40b9c30
sicmundu <grodstrike@gmail.com>
2024-06-27 13:54:55 +0700

    update ritual

--- ritual/monitor_logs.sh
+++ ritual/monitor_logs.sh
@@ -5,31 +5,22 @@ log_file="/tmp/ritual_log.txt"
 
 # Бесконечный цикл для мониторинга
 while true; do
-    # Проверяем, есть ли активная сессия screen с именем ritual
-    if screen -list | grep -q "ritual"; then
-        # Создаем жесткую копию содержимого screen в файл
-        screen -S ritual -X hardcopy $log_file
-        
-        # Получаем последние 100 строк из лога для анализа
-        log_output=$(tail -n 100 $log_file)
-        
-        # Проверяем условия для перезапуска по содержимому лога
-        if [[ "$log_output" == *"[error    ] Task exited: {'code': -32000, 'message': 'filter not found'} [__main__]"* ]] || 
-           [[ "$log_output" == *"Exited main process"* ]]; then
-            echo "Detected critical error in logs. Restarting Docker containers..."
-             docker restart infernet-anvil
-            docker restart infernet-node
-            docker restart hello-world
-            docker restart deploy-node-1
-            docker restart deploy-fluentbit-1
-            docker restart deploy-redis-1
-        fi
-    else
-        echo "Screen session 'ritual' not found. Checking again in 60 seconds..."
-        # Здесь можно добавить команды для запуска сессии screen, если это необходимо
+    # Получаем последние 100 строк логов контейнера infernet-node
+    docker logs --tail 100 infernet-node > $log_file
+    log_output=$(cat $log_file)
+    
+    # Проверяем условия для перезапуска по содержимому лога
+    if [[ "$log_output" == *"[error    ] Task exited: {'code': -32000, 'message': 'filter not found'} [__main__]"* ]] || 
+       [[ "$log_output" == *"Exited main process"* ]]; then
+        echo "Detected critical error in logs. Restarting Docker containers..."
+        docker restart infernet-anvil
+        docker restart infernet-node
+        docker restart hello-world
+        docker restart deploy-node-1
+        docker restart deploy-fluentbit-1
+        docker restart deploy-redis-1
     fi
 
     # Пауза перед следующей итерацией цикла
     sleep 60
 done
-
